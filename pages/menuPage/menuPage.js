55bb6bf5fa256dbc676d445d8ee8a33fe39a3aa9
unknown <zhangzhijian@corp.netease.com>
2018-06-10 19:14:09 +0800

    menuPage 4.0

--- pages/menuPage/menuPage.js
+++ pages/menuPage/menuPage.js
@@ -9,6 +9,148 @@ Page({
         order_view_height: 100,
         show_order: false,
         classListStyle: 'class-list',
+        table_list: [
+            {
+                id: "A1",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A2",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A3",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A4",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A5",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A6",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A7",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A8",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A9",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A10",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A11",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "A12",
+                status: "订",
+                color: 1,
+            },
+            {
+                id: "B1",
+                status: "订",
+                color: 2,
+            },
+            {
+                id: "B2",
+                status: "订",
+                color: 2,
+            },
+            {
+                id: "B3",
+                status: "订",
+                color: 2,                
+            },
+            {
+                id: "B4",
+                status: "订",
+                color: 2,
+            },
+            {
+                id: "B5",
+                status: "订",
+                color: 2,
+            },
+            {
+                id: "B6",
+                status: "订",
+                color: 2,
+            },
+            {
+                id: "B7",
+                status: "订",
+                color: 2,
+            },
+            {
+                id: "B8",
+                status: "订",
+                color: 2,
+            },
+            {
+                id: "C1",
+                status: "订",
+                color: 3,
+            },
+            {
+                id: "C2",
+                status: "订",
+                color: 3,
+            },
+            {
+                id: "C3",
+                status: "订",
+                color: 3,
+            },
+            {
+                id: "C4",
+                status: "订",
+                color: 3,
+            },
+            {
+                id: "C5",
+                status: "订",
+                color: 3,
+            },
+            {
+                id: "C6",
+                status: "订",
+                color: 3,
+            },
+            {
+                id: "C7",
+                status: "订",
+                color: 3,
+            },
+            {
+                id: "C8",
+                status: "订",
+                color: 3,
+            }
+        ],
         imgUrls: [
             'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
             'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
@@ -119,12 +261,14 @@ Page({
     },
 
     showOrderMenu: function(e) {
+      //struct orderedItem
       function orderedItem() {
         this.dish_id = 0;
         this.dish_name = "";
         this.price = 0;
         this.amount = 0;
       }
+
       var change = false;
       if (!this.data.show_order) {
         change = true;
